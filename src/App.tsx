7bdbed1fe850ec71f7885c694147b57d1c36a6e4
heartggs <jounghj0806@naver.com>
2023-06-14 14:16:13 +0900

    팝업리스트 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useEffect, useState } from 'react'
-import "./App.css"
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Review from './pages/Review';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Review from "./pages/Review";
+import PopupListPage from "./pages/PopupListPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() { 
-
+function App() {
   return (
-    <Review />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/popupList" element={<PopupListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
-export default App
+export default App;
