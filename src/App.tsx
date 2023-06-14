6def401f3b0a6946dc9e0cae3b6625c19fd4559b
heartggs <jounghj0806@naver.com>
2023-06-14 19:15:38 +0900

    충돌수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Review from "./pages/Review";
+import MainPage from "./pages/MainPage";
+import SellerRegis from "./pages/SellerRegis";
 import PopupListPage from "./pages/PopupListPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      {/* <SellerRegis /> */}
       <PopupListPage />
     </>
   );
