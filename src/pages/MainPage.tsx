1acc526af6ed3448f377bfb06d2295677bf7568c
Choi A-ri <ari3253@naver.com>
2023-06-21 19:43:55 +0900

    update mainpage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,17 +3,17 @@ import RecomList from "../components/RecomList";
 
 const MainPage = () => {
   return (
-    <div className="">
-      <div className="navbar bg-base-100">
+    <div className="w-4/5 my-0 mx-auto">
+      <div className="navbar bg-base-100 border-b">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <div className="dropdown z-50">
+            <label tabIndex={0} className="btn btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><a>Item 1</a></li>
               <li>
-                <a>Parent</a>
+                <a>Item 2</a>
                 <ul className="p-2">
                   <li><a>Submenu 1</a></li>
                   <li><a>Submenu 2</a></li>
@@ -24,26 +24,11 @@ const MainPage = () => {
           </div>
           <a className="btn btn-ghost normal-case text-xl">POPIT</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-        <div className="navbar-end">
+        {/* <div className="navbar-end">
           <a className="btn">Button</a>
-        </div>
+        </div> */}
       </div>
-      <div>
+      <div className="">
         <input type="text" placeholder="검색하세요!" className="input input-bordered w-full max-w-xs" />
       </div>
       <div className="card w-96 bg-base-100 shadow-xl image-full w-full">
@@ -58,14 +43,14 @@ const MainPage = () => {
       </div>
       <div>
         <div className="flex flex-col w-full">
-          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
+          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">팝업스토어 보기</div> 
           <div className="divider"></div> 
-          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+          <div className="grid h-20 card bg-base-300 rounded-box place-items-center">셀러 등록하러 가기</div>
         </div>
       </div>
-      {[...Array(5)].map((_, index) => (
-        <RecomList key={index} />
-      ))}
+
+      <RecomList />
+
       <Footer />
     </div>
   );
