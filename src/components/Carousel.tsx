7b1192b3d3cb73377e5980087577ab0eae8e67d6
Choi A-ri <ari3253@naver.com>
2023-06-24 02:56:34 +0900

    add carousel

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,85 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+function CarouselComponent() {
+
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(true);
+
+  useEffect(() => {
+    const message = "반갑습니다:)\nPOPIT!에서 다양한 팝업스토어를 둘러보세요.";
+    let currentIndex = 0;
+    let timeout: string | number | NodeJS.Timeout | undefined;
+
+    const type = () => {
+      if (currentIndex < message.length) {
+        setText(message.substring(0, currentIndex + 1));
+        currentIndex++;
+        timeout = setTimeout(type, 100);
+      } else {
+        setIsTyping(false);
+        clearTimeout(timeout);
+      }
+    };
+
+    type();
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const typingText = isTyping ? (
+    <span className="inline-block">
+      {text.split('\n').map((line, index) => (
+        <span key={index}>
+          {line}
+      {/* <span className="animate-blink">|</span> */}
+          <br />
+        </span>
+      ))}
+    </span>
+  ) : (
+    <span>
+      {text.split('\n').map((line, index) => (
+        <span key={index}>
+          {line}
+          <br />
+        </span>
+      ))}
+    </span>
+  );
+
+  return (
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+      <div className="w-full sm:w-96 md:w-120 lg:w-1/2">
+        <Carousel showArrows={false} showStatus={false} showThumbs={false} autoPlay infiniteLoop>
+          <div>
+            <img src="../../src/assets/images/KakaoTalk_Photo_2023-06-23-22-43-57 002.jpeg" />
+          </div>
+          <div>
+            <img src="../../src/assets/images/KakaoTalk_Photo_2023-06-23-22-43-57 004.jpeg" />
+          </div>
+          <div>
+            <img src="../../src/assets/images/KakaoTalk_Photo_2023-06-23-22-43-57 006.jpeg" />
+          </div>
+          <div>
+            <img src="../../src/assets/images/KakaoTalk_Photo_2023-06-23-22-43-56 001.jpeg" />
+          </div>
+        </Carousel>
+      </div>
+      <div className="card-body">
+        <p className="card-title">{typingText}</p>
+        <div className="card-actions justify-end">
+        <Link to="/popuplist">
+          <button className="btn btn-outline mb-5 border-indigo-400 text-indigo-400 hover:bg-indigo-400 hover:text-white hover:border-indigo-400" >
+            팝업스토어 보기
+          </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CarouselComponent;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,19 +4,37 @@ import { FiSearch } from 'react-icons/Fi';
 import { AiOutlineUser } from 'react-icons/Ai';
 import { Link } from "react-router-dom";
 import Navbar from "../components/MainNav";
+import CarouselComponent from "../components/Carousel";
+import { useState } from "react";
 
 
 const MainPage = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = () => {
+    console.log('Searching for:', searchValue);
+  };
+
   return (
     <>
       <div className="max-w-screen-lg my-0 mx-auto">
         <div className="my-0 mx-auto">
-        <Navbar />
-          <div className="flex">
-          <FiSearch />
-            <input type="text" placeholder="스토어나 동네를 검색하세요!" className="input input-bordered w-full max-w-xs" />
+          <Navbar />
+          <div className="flex justify-center mt-5 mb-1">
+            <div className="items-center justify-center flex m-2" onClick={handleSearch}>
+              <FiSearch size="28" color="#a5b4fc" />
+            </div>
+            <input
+              type="text"
+              placeholder="스토어나 동네를 검색하세요!"
+              className="px-4 py-2 w-64 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-indigo-300"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl image-full w-full">
+          <CarouselComponent />
+          {/* <div className="card w-96 bg-indigo-200 shadow-xl image-full w-full"> */}
+          <div className="card w-96 bg-indigo-200 shadow-xl w-full mt-8 mb-8">
             <div className="card-body">
               <h2 className="card-title">POPIT!</h2>
               <p>내 주변 팝업 스토어 찾기</p>
@@ -31,12 +49,12 @@ const MainPage = () => {
           </div>
           <div>
             <div className="flex flex-col w-full">
-              <Link to="popuplist">
-              <div className="btn grid h-20 card bg-base-300 rounded-box place-items-center">팝업스토어 보기</div> 
+              <Link to="/profile">
+              <div className="btn grid h-20 card bg-base-300 rounded-box place-items-center">셀러 등록하러 가기</div> 
               </Link>
               <div className="divider"></div> 
-              <Link to="profile">
-              <div className="btn grid h-20 card bg-base-300 rounded-box place-items-center">셀러 등록하러 가기</div>
+              <Link to="/cart">
+              <div className="btn grid h-20 card bg-base-300 rounded-box place-items-center">장바구니 가기</div>
               </Link>
             </div>
           </div>
