1a377b5c12868056d4189073c3559044a4e82def
steeringhead <riceqjrj0367@gmail.com>
2023-06-25 19:06:25 +0900

    추가 page

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,21 @@ import ProductRegis from "./pages/ProductRegis";
 import { RecoilRoot } from "recoil";
 import MapPage from "./pages/MapPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FindId from "./pages/FindId";
+import FindPassword from "./pages/FindPassword";
 
 function App() {
   return (
     <RecoilRoot>
-      <SellerRegis />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SellerRegis />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/findId" element={<FindId />} />
+          <Route path="/findId" element={<FindId />} />
+          <Route path="/findPassword" element={<FindPassword />} />
+        </Routes>
+      </BrowserRouter>      
     </RecoilRoot>
   );
 }
